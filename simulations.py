b4610ef36565bd0785c235d1bd6fad0081e1a611
eytangf4 <eytangf@gmail.com>
2024-07-09 10:39:58 -0700

    shave sphere from cell

--- simulations.py
+++ simulations.py
@@ -1,9 +1,22 @@
 from ovito.io import import_file
-from ovito.modifiers import ReplicateModifier
-import numpy
+from ovito.modifiers import ReplicateModifier, DeleteSelectedModifier
+import math
 
 pipeline = import_file("/Users/eytangf/Desktop/Internship/Nanoparticle Simulations/Fe2O3.cif")
 pipeline.modifiers.append(ReplicateModifier(num_x=20, num_y=20, num_z=10))
 
 data = pipeline.compute()
-positions = data.particles.positions
+# cast as numpy array using [...]
+cell_data = data.cell[...]
+origin = cell_data[:,3]
+radius = 30
+print(origin)
+
+for idx in range (len(data.particles)):
+    particle_position = data.particles.positions[idx]
+  # math.dist is applying the distance formula, aka sqrt((x2-x1)^2 + (y2-y1)^2)
+    distance_from_center = math.dist(particle_position, origin)
+    if distance_from_center > radius:
+        pipeline.modifiers.append(DeleteSelectedModifier(data.particles_[idx]))
+
+
