266e388d9558c5e73f3d53e85d5ed2148479312e
eytangf4 <eytangf@gmail.com>
2024-07-10 15:11:17 -0700

    calculate how many ions (either ferric iron or oxide) needed to balance charge

--- netcharge.py
+++ netcharge.py
@@ -23,9 +23,14 @@ def calculate_net_charge(path):
     oxide_charge = num_oxide * 2
 
     net_charge = ferric_iron_charge - oxide_charge
-    if net_charge > 0:
-        net_charge = f"+{net_charge}"
 
     print(f"Number of Fe3+ atoms: {num_ferric_iron}")
     print(f"Number of O2- atoms: {num_oxide}")
-    print(f"Net Charge: {net_charge}")
+    if net_charge > 0:
+        print(f"Net Charge: +{net_charge}")
+        num_oxide_for_neutral_charge = net_charge/2
+        print(f"{num_oxide_for_neutral_charge} oxide ions needed to create neutrally-charged nanoparticle")
+    else:
+        print(f"Net Charge: {net_charge}")
+        num_ferric_iron_for_neutral_charge = (net_charge/3) * -1
+        print(f"{num_ferric_iron_for_neutral_charge} ferric iron ions needed to create neutrally-charged nanoparticle")
