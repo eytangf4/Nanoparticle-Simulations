ca87a71405485cb787030d9e9d182b562352d719
eytangf4 <eytangf@gmail.com>
2024-07-16 13:21:12 -0700

    do a charge density check for different cutoffs

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ nanoparticles = set_up_nanoparticles("/Users/eytangf/Desktop/Internship/Nanopart
 
 save_lmp_data(nanoparticles, "nanoparticles.lmp")
 
-nanoparticles_with_charge_density = check_charge_density(nanoparticles, cutoff=5)
-
-save_lmp_dump(nanoparticles, "nanoparticles_with_charge_density.lmp")
-
 sphere = unit_cell_to_sphere("/Users/eytangf/Desktop/Internship/Nanoparticle Simulations/Fe2O3.cif", 30)
 print("SPHERE")
 calculate_net_charge(sphere)
@@ -17,6 +13,16 @@ print("\n")
 print("NEUTRALIZED SPHERE")
 calculate_net_charge(neutralized_sphere)
 save_lmp_data(neutralized_sphere, "neutralized_sphere.lmp")
+
+neutralized_sphere_charge_density_check_cutoff_five = check_charge_density(neutralized_sphere, cutoff=5)
+print("\n")
+print("CHARGE DENSITY CHECK")
+save_lmp_dump(neutralized_sphere_charge_density_check_cutoff_five, "neutralized_sphere_charge_density_check_cutoff_five.lmp")
+
+neutralized_sphere_charge_density_check_cutoff_ten = check_charge_density(neutralized_sphere, cutoff=10)
+print("\n")
+print("CHARGE DENSITY CHECK")
+save_lmp_dump(neutralized_sphere_charge_density_check_cutoff_ten, "neutralized_sphere_charge_density_check_cutoff_ten.lmp")
 
 rotated_sphere = rotate_sphere(neutralized_sphere, azimuth=math.pi/6, elevation=math.pi/3)
 print("\n")
